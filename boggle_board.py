a46bce78dceab12d8bef01d3f0bba6ea5f1e687b
KevinBaoB <kevinbaobui@gmail.com>
2022-06-03 08:01:19 -0500

    First still working

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,31 @@
+import random
+import string
+
 class BoggleBoard:
+
   
   def __init__(self):
-  
+    self.game_board = ['_'] * 16
+    
+    
+
 
   def shake(self):
+    
+    for i, under_score in enumerate(self.game_board):
+      random_letter = random.choice(string.ascii_letters)
+      self.game_board[i] = random_letter
+
+
+  def view_game_board(self):
+    return self.game_board
+
+  def print_board(self):
+    for i in range(0, len(self.game_board), 4):
+      print("".join(self.game_board[i:i+4]))
+
+  
+
+boggle = BoggleBoard()
+boggle.shake()
+boggle.print_board()
